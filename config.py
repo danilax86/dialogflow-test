755ccc0be128d1b71f0903511bd031b092cb684a
Danila Gorelko <danila@danilax86.ru>
2021-07-14 12:12:01 +0300

    add config.py

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ class Config(object):
     DEVELOPMENT = False
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
-    MAIL_USERNAME = 'MAIL'
-    MAIL_PASSWORD = 'PASS'
+    MAIL_USERNAME = '<your_mail>@gmail.com'
+    MAIL_PASSWORD = '<your_gmail_pass>'
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    TWILIO_SID = ''
-    TWILIO_TOKEN = ''
+    TWILIO_SID = '<your_twilio_SID>'
+    TWILIO_TOKEN = '<your_twilio_token>'
+    TWILIO_NUM = '<your_twilio_number>'
