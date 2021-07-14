49bc5ca0b1648f0461802cdc3621aec954ef7836
Danila Gorelko <danila@danilax86.ru>
2021-07-14 11:47:41 +0300

    add sending sms

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response, jsonify, json
 from flask_mail import Mail, Message
 from datetime import datetime
+from twilio.rest import Client
 
 import config
 
@@ -8,8 +9,11 @@ app = Flask(__name__)
 app.config.from_object(config.Config)
 mail = Mail(app)
 
+twilio_num = '+18327261645'
+client = Client(app.config['TWILIO_SID'], app.config['TWILIO_TOKEN'])
 
-def send_msg(sender, recipient, subject, text):
+
+def send_email_msg(sender, recipient, subject, text):
     msg = Message(
         subject = subject,
         sender = sender,
@@ -17,6 +21,14 @@ def send_msg(sender, recipient, subject, text):
         html = text
     )
     mail.send(msg)
+
+
+def send_sms(recipient_num, text):
+    client.messages.create(
+        to = f'+{recipient_num}',
+        from_ = twilio_num,
+        body = text
+    )
 
 
 def results():
@@ -30,10 +42,12 @@ def results():
     time = time.time()
     email = req.get('queryResult').get('parameters').get('email')
     name = req.get('queryResult').get('parameters').get('name')
+    recipient_num = int(req.get('queryResult').get('parameters').get('number'))
 
-    # send an email msg
-    send_msg(app.config['MAIL_USERNAME'], email, 'Appointment',
-             f'<p> Hello, {name}. Your appointment is scheduled for {day} at {time} </p>')
+    text = f'Hello, {name}. Your appointment is scheduled for {day} at {time}'
+
+    send_email_msg(app.config['MAIL_USERNAME'], email, 'Appointment', text)
+    send_sms(recipient_num, text)
 
     # return a fulfillment response
     return {'fulfillmentText': 'This is a response from webhook.'}
